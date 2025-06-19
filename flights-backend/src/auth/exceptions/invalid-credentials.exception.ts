6e8d0e89c7f5d5bf2d7680f267efdc6d249b01ec
senseei <gabrieldjesus475@gmail.com>
2025-06-19 17:35:19 -0300

    feat(backend): add InvalidBodyException and update InvalidCredentialsException for improved error handling

--- flights-backend/src/auth/exceptions/invalid-credentials.exception.ts
+++ flights-backend/src/auth/exceptions/invalid-credentials.exception.ts
@@ -1,6 +1,10 @@
-export class InvalidCredentialsException extends Error {
-  constructor(message: string = 'Invalid credentials') {
-    super(message);
-    this.name = 'InvalidCredentialsException';
+import { HttpStatus } from "@nestjs/common";
+
+import { CustomException } from "@common/exceptions/custom.exception";
+
+export class InvalidCredentialsException extends CustomException {
+
+  constructor(message: string) {
+    super(message, HttpStatus.UNAUTHORIZED);
   }
 }
