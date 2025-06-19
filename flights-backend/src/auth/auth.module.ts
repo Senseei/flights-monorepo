4494147f57dbc3bcac3e4f3b8e5b52658831d265
senseei <gabrieldjesus475@gmail.com>
2025-06-19 17:08:57 -0300

    feat(backend): add AuthService to AuthModule providers

--- flights-backend/src/auth/auth.module.ts
+++ flights-backend/src/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { PassportModule } from '@nestjs/passport';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { JwtStrategy } from './jwt.strategy';
+import { AuthService } from './auth.service';
 
 @Module({
   imports: [
@@ -17,7 +18,7 @@ import { JwtStrategy } from './jwt.strategy';
       inject: [ConfigService],
     }),
   ],
-  providers: [JwtStrategy],
+  providers: [JwtStrategy, AuthService],
   controllers: [],
 })
 export class AuthModule {}
