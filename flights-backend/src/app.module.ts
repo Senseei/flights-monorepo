59b56e19399c06121069a4e458757542e8ff69c8
senseei <gabrieldjesus475@gmail.com>
2025-06-19 17:36:01 -0300

    feat(backend): integrate ConfigModule and CommonModule into AppModule

--- flights-backend/src/app.module.ts
+++ flights-backend/src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppService } from './app.service';
+
+import { AppController } from './app.controller';
+
 import { DatabaseModule } from './database/database.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from '@auth/auth.module';
+import { UsersModule } from '@users/users.module';
+import { CommonModule } from '@common/common.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, UsersModule],
+  imports: [ConfigModule.forRoot(), DatabaseModule, UsersModule, AuthModule, CommonModule],
   controllers: [AppController],
   providers: [AppService],
 })
