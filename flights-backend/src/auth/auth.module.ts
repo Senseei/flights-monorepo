eac5db492bce01e1b4576e52319e55b851433d3c
senseei <gabrieldjesus475@gmail.com>
2025-06-19 14:28:19 -0300

    feat(backend): implement AuthModule with JWT strategy for authentication

---
+++ flights-backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '8h' },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [JwtStrategy],
+  controllers: [],
+})
+export class AuthModule {}
