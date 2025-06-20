0e12ed3c8984956c926b061d1b43bfea0ab688ac
senseei <gabrieldjesus475@gmail.com>
2025-06-20 13:36:02 -0300

    feat(backend): create DomainEntity class with common timestamp fields

--- flights-backend/src/users/entities/user.entity.ts
+++ flights-backend/src/users/entities/user.entity.ts
@@ -1,28 +1,24 @@
 import { Column, Entity, PrimaryGeneratedColumn, Unique } from "typeorm";
+import { DomainEntity } from '@common/domain.entity';
 
 @Entity("tb_users")
 @Unique(["email"])
-export class User {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class User extends DomainEntity {
   @Column()
   name: string;
   @Column()
   email: string;
   @Column()
   password: string;
-  @Column({ name: "created_at" })
-  createdAt: Date;
 
   constructor(
     name: string,
     email: string,
     password: string
   ) {
+    super();
     this.name = name;
     this.email = email;
     this.password = password;
-    this.createdAt = new Date();
   }
 }
