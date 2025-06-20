18bf56f9b429f2a39765e397ecb6d9d9b584926e
senseei <gabrieldjesus475@gmail.com>
2025-06-20 15:12:10 -0300

    feat(backend): implement findAll method in FlightsService to retrieve all flights

--- flights-backend/src/flights/flights.service.ts
+++ flights-backend/src/flights/flights.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { FlightsRepository } from '@flights/flights.repository';
+import { FlightDTO } from '@flights/dtos/FlightDTO';
 
 @Injectable()
-export class FlightsService {}
+export class FlightsService {
+  constructor(private readonly repository: FlightsRepository) {}
+
+  public async findAll(): Promise<FlightDTO[]> {
+    return (await this.repository.findAll()).map(it => new FlightDTO(it));
+  }
+}
