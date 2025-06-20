cfd9d0381ddd624bb70db26f7323a6d0a4998613
senseei <gabrieldjesus475@gmail.com>
2025-06-20 13:05:11 -0300

    fix(backend): update database configuration to use new environment variable names and rename postgres service in docker-compose

--- flights-backend/src/database/database.module.ts
+++ flights-backend/src/database/database.module.ts
@@ -10,10 +10,10 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       useFactory: (configService: ConfigService) => ({
         type: 'postgres',
         host: configService.get('DB_HOST'),
-        port: parseInt(<string>configService.get('POSTGRES_PORT'), 10),
-        username: configService.get('POSTGRES_USER'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        database: configService.get('POSTGRES_DB'),
+        port: parseInt(<string>configService.get('DB_PORT'), 10),
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
         autoLoadEntities: true,
         synchronize: configService.get('NODE_ENV') === 'development', // Sync only in development
         migrations: [`${__dirname}/migrations/*{.ts,.js}`],
