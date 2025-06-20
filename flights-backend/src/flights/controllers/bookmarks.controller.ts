d20ac5aad8b4ec2f7c89af0e5f936a3d5d8b5192
senseei <gabrieldjesus475@gmail.com>
2025-06-20 17:25:01 -0300

    feat(backend): add methods for adding and removing flights from bookmarks

--- flights-backend/src/flights/controllers/bookmarks.controller.ts
+++ flights-backend/src/flights/controllers/bookmarks.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '@auth/jwt-auth.guard';
 import { FlightsService } from '@flights/flights.service';
 import { FlightDTO } from '@flights/dtos/FlightDTO';
@@ -11,5 +11,17 @@ export class BookmarksController {
   @Get()
   public async findAll(@Req() request: any): Promise<FlightDTO[]> {
     return this.service.findAllBookmarksByUserId(parseInt(request.user.id));
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async addFlightToBookmarks(@Req() request: any, @Body() body: { flightId: number }): Promise<void> {
+    await this.service.addFlightToBookmarks(parseInt(request.user.id), body.flightId);
+  }
+
+  @Delete(':flightId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  public async removeFlightFromBookmarks(@Req() request: any, @Param("flightId") flightId: number): Promise<void> {
+    await this.service.removeFlightFromBookmarks(parseInt(request.user.id), flightId);
   }
 }
--- flights-backend/src/flights/flights.service.ts
+++ flights-backend/src/flights/flights.service.ts
@@ -3,6 +3,7 @@ import { FlightsRepository } from '@flights/flights.repository';
 import { FlightDTO } from '@flights/dtos/FlightDTO';
 import { UsersRepository } from '@users/users.repository';
 import { EntityNotFoundException } from '@common/exceptions/entity-not-found.exception';
+import { FlightAlreadyBookmarkedException } from '@flights/exceptions/flight-already-bookmarked.exception';
 
 @Injectable()
 export class FlightsService {
@@ -21,5 +22,39 @@ export class FlightsService {
       throw new EntityNotFoundException('User', id);
     }
     return user.bookmarks.map((it) => new FlightDTO(it));
+  }
+
+  public async addFlightToBookmarks(userId: number, flightId: number): Promise<void> {
+    const user = await this.usersRepository.findById(userId);
+    if (!user) {
+      throw new EntityNotFoundException('User', userId);
+    }
+
+    const flight = await this.repository.findById(flightId);
+    if (!flight) {
+      throw new EntityNotFoundException('Flight', flightId);
+    }
+
+    if (user.bookmarks.some((bookmark) => bookmark.id === flightId)) {
+      throw new FlightAlreadyBookmarkedException(flightId);
+    }
+
+    user.bookmarks.push(flight);
+    await this.usersRepository.save(user);
+  }
+
+  public async removeFlightFromBookmarks(userId: number, flightId: number): Promise<void> {
+    const user = await this.usersRepository.findById(userId);
+    if (!user) {
+      throw new EntityNotFoundException('User', userId);
+    }
+
+    const flightIndex = user.bookmarks.findIndex((bookmark) => bookmark.id === flightId);
+    if (flightIndex === -1) {
+      throw new EntityNotFoundException('Flight', flightId);
+    }
+
+    user.bookmarks.splice(flightIndex, 1);
+    await this.usersRepository.save(user);
   }
 }
