baf637d2e4b71608db9fdfd907152b3679f38c14
senseei <gabrieldjesus475@gmail.com>
2025-06-21 02:33:19 -0300

    feat(backend): update findAll method to support pagination

--- flights-backend/src/common/common.repository.ts
+++ flights-backend/src/common/common.repository.ts
@@ -1,5 +1,6 @@
 import { FindOptionsWhere, Repository } from 'typeorm';
 import { DomainEntity } from '@common/domain.entity';
+import { PaginatedResult } from '@common/paginated-result';
 
 export abstract class CRUDRepository<E extends DomainEntity> {
   protected constructor(protected readonly repository: Repository<E>) {}
@@ -10,8 +11,14 @@ export abstract class CRUDRepository<E extends DomainEntity> {
     });
   }
 
-  public async findAll(): Promise<E[]> {
-    return this.repository.find();
+  public async findAll(page: number, limit: number, filters: FindOptionsWhere<E>): Promise<PaginatedResult<E>> {
+    const skip = (page - 1) * limit;
+    const [entities, total] = await this.repository.findAndCount({
+      where: filters,
+      skip,
+      take: limit,
+    });
+    return new PaginatedResult<E>(entities, total);
   }
 
   public async save(entity: E): Promise<E> {
