94c57318c91762b932094f848e187a04434152bc
senseei <gabrieldjesus475@gmail.com>
2025-06-21 01:52:34 -0300

    feat(backend): add DTOs for pagination handling

---
+++ flights-backend/src/common/dtos/paginated-result.dto.ts
@@ -0,0 +1,13 @@
+export class PaginatedResultDTO<T> {
+  items: T[];
+  totalCount: number;
+  pageSize: number;
+  currentPage: number;
+
+  constructor(items: T[], totalCount: number, pageSize: number, currentPage: number) {
+    this.items = items;
+    this.totalCount = totalCount;
+    this.pageSize = pageSize;
+    this.currentPage = currentPage;
+  }
+}
---
+++ flights-backend/src/common/dtos/pagination-params.dto.ts
@@ -0,0 +1,19 @@
+import { Type } from "class-transformer";
+import { IsInt, IsOptional, Min } from "class-validator";
+
+export class PaginationParamsDTO {
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  page = 1;
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Type(() => Number)
+  limit = 10;
+
+  public getOffset(): number {
+    return (this.page - 1) * this.limit;
+  }
+}
