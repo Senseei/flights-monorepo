89cf806de66ee1b8c581459411f2cfd86028b9ce
senseei <gabrieldjesus475@gmail.com>
2025-06-21 12:23:53 -0300

    feat(backend): enhance findAll method to support sorting options

--- flights-backend/src/common/common.repository.ts
+++ flights-backend/src/common/common.repository.ts
@@ -1,4 +1,4 @@
-import { FindOptionsWhere, Repository } from 'typeorm';
+import { FindOptionsOrder, FindOptionsWhere, Repository } from 'typeorm';
 import { DomainEntity } from '@common/domain.entity';
 import { PaginatedResult } from '@common/paginated-result';
 
@@ -11,12 +11,21 @@ export abstract class CRUDRepository<E extends DomainEntity> {
     });
   }
 
-  public async findAll(page: number, limit: number, filters: FindOptionsWhere<E>): Promise<PaginatedResult<E>> {
+  public async findAll(
+    page: number,
+    limit: number,
+    filters: FindOptionsWhere<E>,
+    sortField: keyof E = 'createdAt',
+    sortOrder: 'ASC' | 'DESC' = 'DESC',
+  ): Promise<PaginatedResult<E>> {
     const skip = (page - 1) * limit;
     const [entities, total] = await this.repository.findAndCount({
       where: filters,
       skip,
       take: limit,
+      order: {
+        [sortField]: sortOrder
+      } as FindOptionsOrder<E>,
     });
     return new PaginatedResult<E>(entities, total);
   }
