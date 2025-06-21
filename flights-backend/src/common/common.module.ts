fd72d6af385e5c33c6dcdbf4af58ad44cea8588d
senseei <gabrieldjesus475@gmail.com>
2025-06-21 13:16:00 -0300

    feat(backend): add EntityValidatorService for safe entity key retrieval

--- flights-backend/src/common/common.module.ts
+++ flights-backend/src/common/common.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { EntityValidatorService } from './entity-validator.service';
 
-@Module({})
+@Module({
+  providers: [EntityValidatorService],
+  exports: [EntityValidatorService],
+})
 export class CommonModule {}
